c1f5ded78ed854af065b33f5ea43d2745c80855c
Nick1st <nick1st.dev@gmail.com>
2022-05-14 17:44:40 +0200

    Try implementation of a new Buffer

--- Common/src/main/java/nick1st/fancyvideo/FancyVideoEvents.java
+++ Common/src/main/java/nick1st/fancyvideo/FancyVideoEvents.java
@@ -4,6 +4,7 @@ import nick1st.fancyvideo.api.DynamicResourceLocation;
 import nick1st.fancyvideo.api.eventbus.EventPriority;
 import nick1st.fancyvideo.api.eventbus.FancyVideoEvent;
 import nick1st.fancyvideo.api.eventbus.event.PlayerRegistryEvent;
+import nick1st.fancyvideo.api.internal.utils.IntegerBuffer2D;
 import nick1st.fancyvideo.api.mediaPlayer.SimpleMediaPlayer;
 import nick1st.fancyvideo.internal.SimpleTextRenderer;
 import nick1st.fancyvideo.internal.Util;
@@ -18,6 +19,10 @@ public record FancyVideoEvents() {
         ((SimpleMediaPlayer) event.handler().getMediaPlayer(fallback)).setAdvancedFrame(Util.injectableTextureFromJar("VLCMissing.png", FancyVideoEvent.class.getClassLoader(), 1024));
         //((SimpleMediaPlayer) event.handler().getMediaPlayer(fallback)).setAdvancedFrame(Util.injectableTextureFromJar("assets/minecraft/textures/font/ascii.png", FancyVideoEvent.class.getClassLoader(), 128));
         ((SimpleMediaPlayer) event.handler().getMediaPlayer(fallback)).renderToResourceLocation();
-        SimpleTextRenderer.getInstance().drawString("Test", 0, 1.0F, 5);
+
+        //SimpleTextRenderer.getInstance().drawString("Test", 0, 1.0F, 5);
+
+        IntegerBuffer2D testBuffer = new IntegerBuffer2D(10, 10);
+        testBuffer.fill(100);
     }
 }
--- Common/src/main/java/nick1st/fancyvideo/api/internal/utils/IntegerBuffer2D.java
+++ Common/src/main/java/nick1st/fancyvideo/api/internal/utils/IntegerBuffer2D.java
@@ -13,13 +13,66 @@ public class IntegerBuffer2D {
         Arrays.fill(this.banks, IntBuffer.allocate(sizeOfBanks));
     }
 
-    public void growBanks(int sizeToGrow) {
-        int oldLength = banks.length;
-        banks = Arrays.copyOf(banks, oldLength + sizeToGrow);
-        Arrays.fill(banks, oldLength, oldLength + sizeToGrow, IntBuffer.allocate(banks[0].limit()));
+    public void grow(int sizeToGrowX, int sizeToGrowY) {
+        if (sizeToGrowY > 0) {
+            int oldLength = banks.length;
+            banks = Arrays.copyOf(banks, oldLength + sizeToGrowY);
+            Arrays.fill(banks, oldLength, oldLength + sizeToGrowY, IntBuffer.allocate(banks[0].limit()));
+        }
+        if (sizeToGrowX > 0) {
+            for (int i = 0; i < banks.length; i++) {
+                banks[i] = IntBuffer.wrap(banks[i].array());
+            }
+        }
     }
 
     public void bulkPut(AdvancedFrame[] frameArray) {
 
+    }
+
+    public void bulkPut(IntegerBuffer2D bufferToOverlay, int startX, int startY) {
+        bulkPut(bufferToOverlay, startX, startY, true);
+    }
+
+    public void bulkPut(IntegerBuffer2D bufferToOverlay, int startX, int startY, boolean skipZeros) {
+        int k = 0;
+        for (int i = startY; i < bufferToOverlay.banks.length; i++) {
+            for (int j = startX; j < bufferToOverlay.banks[0].array().length; j++) {
+                int value = bufferToOverlay.banks[k].get(j);
+                if (value != 0) {
+                    banks[i].put(j, value);
+                }
+            }
+            k++;
+        }
+    }
+
+    public void fill(int value) {
+        fill(value, true);
+    }
+
+    public void fill(int value, boolean onlyZeros) {
+        for (int i = 0; i < banks.length; i++) {
+            for (int j = 0; j < banks[0].array().length; j++) {
+                if (banks[i].get(j) == 0) {
+                    banks[i].put(j, value);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String s = "IntegerBuffer2D{\n";
+        for (int i = 0; i < banks.length; i++) {
+            int cap = banks[0].capacity();
+            s += "[";
+            for (int j = 0; j < cap; j++) {
+                s += banks[i].get(j);
+                     s += (j != cap - 1) ? "," : "]\n";
+            }
+        }
+        s += "}";
+        return s;
     }
 }
